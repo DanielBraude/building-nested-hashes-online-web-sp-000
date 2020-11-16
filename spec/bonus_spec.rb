2f31d824e5832f6520c71a139775d561e6316ffc
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-11-16 20:34:00 +0000

    Automatically backed up by Learn

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -4,8 +4,4 @@ require_relative '../lib/bonus.rb'
 describe "#bonus" do
   # remove the 'x' from the 'xit' below to activate this test!
 
-    expect(bonus[:montague][:hero][:status]).to eq("dead")
-    expect(bonus[:capulet][:heroine][:status]).to eq("dead")
-
-  end
-end
+  
